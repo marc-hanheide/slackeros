4bde315558e1dd36e392844190ee00c1acd652e2
Marc Hanheide <marc@hanheide.net>
2018-09-16 16:55:33 +0100

    with params

--- slackeros/scripts/slackeros_node.py
+++ slackeros/scripts/slackeros_node.py
@@ -11,9 +11,25 @@ def __signal_handler(signum, frame):
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, __signal_handler)
-    rospy.init_node('slack_ros')
+    rospy.init_node('slackeros')
+    hook = rospy.get_param(
+        '~webhook',
+        'https://hooks.slack.com/services/'
+        'TCTBP6280/BCU8QFBE1/l2B4r7TRzLJJ37zyhXqtICov')
+    wl_users = rospy.get_param(
+        '~users', [])
+    wl_channels = rospy.get_param(
+        '~channels', [])
+    topics = rospy.get_param(
+        '~topics', [])
+    print topics
+    url_prefix = rospy.get_param(
+        '~url_prefix', '')
     sc = RosConnector(
-        incoming_webhook='https://hooks.slack.com/services/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',
-        whitelist_users=['mhanheide']
+        incoming_webhook=hook,
+        whitelist_users=wl_users,
+        whitelist_channels=wl_channels,
+        topics=topics,
+        prefix=url_prefix
     )
     sc.run()
--- slackeros/src/slackeros/ros_connector.py
+++ slackeros/src/slackeros/ros_connector.py
@@ -9,11 +9,14 @@ from std_msgs.msg import String
 from std_srvs.srv import Empty
 from rostopic import get_topic_class
 from rosservice import get_service_class_by_name
+from rosservice import get_service_list, call_service
 from threading import Thread
 from roslib.message import strify_message
 from slackeros.srv import SlashCommand
 import argparse
 from StringIO import StringIO
+import roslib
+import rosmsg
 
 
 def __signal_handler(signum, frame):
@@ -46,6 +49,7 @@ class RosConnector(SlackConnector):
         self.subs = {}
 
         for t in self.topics:
+            print 'topic ', t
             self._subscribe(t)
 
     def _subscribe(self, topic):
@@ -80,6 +84,7 @@ class RosConnector(SlackConnector):
     def _unsubscribe(self, topic):
         if topic in self.subs:
             self.subs[topic].unregister()
+            del self.subs[topic]
 
     def to_slack_cb(self, msg, topic):
         d = strify_message(msg)
@@ -115,11 +120,100 @@ class RosConnector(SlackConnector):
                 return {
                     'attachments': [
                         {
-                            'text': '```\n%s\n```' % '\n'.join(tops),
-                            'author_name': 'ROS master:'
+                            'text': (
+                                '*Currently published topics:*\n```\n%s\n```'
+                                % '\n'.join(tops)),
+                            'author_name': 'ROS master'
+                        },
+                        {
+                            'text': (
+                                '*Currently subscribed by'
+                                ' Slack*:\n```\n%s\n```'
+                                % '\n'.join(self.subs)),
+                            'author_name': 'slackeros'
                         }
                     ],
-                    'text': 'Currently published topics:'
+                    'text': '_Topics:_'
+                }
+            else:
+                return help_string
+
+    def __call_service(self, service_name, service_args, service_class=None):
+        import std_msgs.msg
+
+        if service_class is None:
+            service_class = get_service_class_by_name(service_name)
+        request = service_class._request_class()
+        try:
+            now = rospy.get_rostime()
+            keys = {'now': now, 'auto': std_msgs.msg.Header(stamp=now)}
+            roslib.message.fill_message_args(request, service_args, keys=keys)
+        except roslib.message.ROSMessageException, e:
+            def argsummary(args):
+                if type(args) in [tuple, list]:
+                    return '\n'.join(
+                        [
+                            ' * %s (type %s)' % (a, type(a).__name__)
+                            for a in args])
+                else:
+                    return ' * %s (type %s)' % (args, type(args).__name__)
+
+            return (
+                "Incompatible arguments to call service:\n%s\n"
+                "Provided arguments are:\n%s\n\nService arguments are: [%s]"
+                % (
+                    e, argsummary(service_args),
+                    roslib.message.get_printable_message_args(request)))
+        try:
+            return rospy.ServiceProxy(
+                service_name, service_class)(request)
+        except rospy.ServiceException, e:
+            return str(e)
+        except roslib.message.SerializationError, e:
+            return (
+                "Unable to send request."
+                " One of the fields has an incorrect type:\n"
+                "  %s\n\nsrv file:\n%s"
+                % (
+                    e,
+                    rosmsg.get_srv_text(service_class._type)))
+        except rospy.ROSSerializationException, e:
+            return (
+                "Unable to send request."
+                " One of the fields has an incorrect type:\n"
+                "  %s\n\nsrv file:\n%s" % (
+                    e, rosmsg.get_srv_text(service_class._type)))
+
+    def _rosservice(self, args):
+        help_string = '`usage: /rosservice [call <service> [<args>]|list]`'
+        if len(args) < 1:
+            return help_string
+        else:
+            cmd = args[0].lower()
+            if cmd == 'call':
+                service = args[1]
+                resp = self.__call_service(service, args[2:])
+                return {
+                    'attachments': [
+                        {
+                            'text': 'Response:\n```\n%s\n```' % resp,
+                            'author_name': 'service'
+                        }
+                    ],
+                    'text': '_called `%s`_' % service
+                }
+            elif cmd == 'list':
+                services = get_service_list()
+                return {
+                    'attachments': [
+                        {
+                            'text': (
+                                '*Currently available services:*\n```\n%s\n```'
+                                % '\n'.join(services)),
+                            'author_name': 'ROS master'
+                        }
+                    ],
+                    'text': '_Services:_'
                 }
             else:
                 return help_string
@@ -132,24 +226,30 @@ class RosConnector(SlackConnector):
         elif service == 'rosservice':
             return self._rosservice(args)
         else:
-            return '_unknown command `%s`' % service
-        #     ret = {}
-        #     try:
-        #         srv_class = get_service_class_by_name(service)
-        #         proxy = rospy.ServiceProxy(service, srv_class)
-        #         ret = "```\n%s\n```" % strify_message(proxy.call())
-        #     except Exception as e:
-        #         ret = '*Failed with exception: %s*' % str(e)
-        # return ret
-
-    # def run(self):
-    #     web.application.run(self)
+            args[0] = self.ROS_PREFIX + '/' + service
+            return self._rosservice(args)
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, __signal_handler)
     rospy.init_node('slackeros')
+    hook = rospy.get_param(
+        '~webhook',
+        'https://hooks.slack.com/services/'
+        'TCTBP6280/BCU8QFBE1/l2B4r7TRzLJJ37zyhXqtICov')
+    wl_users = rospy.get_param(
+        '~users', '')
+    wl_channels = rospy.get_param(
+        '~channels', '')
+    topics = rospy.get_param(
+        '~topics', '')
+    print topics
+    url_prefix = rospy.get_param(
+        '~url_prefix', '')
     sc = RosConnector(
-        incoming_webhook='https://hooks.slack.com/services/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',
-        whitelist_users=['mhanheide']
+        incoming_webhook=hook,
+        whitelist_users=wl_users.split(' '),
+        whitelist_channels=wl_channels.split(' '),
+        topics=topics.split(' '),
+        prefix=url_prefix
     )
     sc.run()
